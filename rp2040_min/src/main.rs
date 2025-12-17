96f63d9da16b416a118af501919b033f8f3f7e85
ews0404 <eric.stiers@gmail.com>
2025-12-17 13:42:59 +1300

    pac push

--- rp2040_min/src/main.rs
+++ rp2040_min/src/main.rs
@@ -1,35 +1,31 @@
 #![no_std]
 #![no_main]
 
-const LED_PIN: u32 = 25;
+const LED_PIN: usize = 25;
 
 use panic_halt as _;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
+
+    // just grab everything
     let pac = unsafe { rp2040_pac::Peripherals::steal() };
 
-    // // Configure pin 25 for GPIO
-    // pac.PADS_BANK0.gpio[25].write(|w| {
-    //     // Output Disable off
-    //     w.od().clear_bit();
-    //     // Input Enable on
-    //     w.ie().set_bit();
-    //     w
-    // });
-    // pac.IO_BANK0.gpio[25].gpio_ctrl.write(|w| {
-    //     // Map pin 25 to SIO
-    //     w.funcsel().sio_0();
-    //     w
-    // });
+    // set LED pin function to SIO (should be modify?)
+    pac.IO_BANK0.gpio(LED_PIN).gpio_ctrl().write(|w| {
+        w.funcsel().sio();
+        w
+    });
 
+    // enable LED pin output
     pac.SIO.gpio_oe_set().write(|w| unsafe {
         w.bits(1 << LED_PIN);
         w
     });
 
+    // set LED pin output
     pac.SIO.gpio_out_set().write(|w| unsafe {
-        w.bits(1 << LED_PIN);
+        w.bits(1<<LED_PIN);
         w
     });
 
